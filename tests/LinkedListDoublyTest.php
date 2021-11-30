42828e181e47d8fef9accad667f4e7869c8a8812
Geo <13597665+github-34@users.noreply.github.com>
2021-11-30 15:19:44 -0500

    Added: Doubly Linked List and Tests

---
+++ tests/LinkedListDoublyTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class LinkedListDoublyTest extends TestCase
+{
+
+    public function testCanAddToLinkedListDoubly(): void
+    {
+        $ll1 = new LinkedListDoubly();
+        $ll1->addFirst(4);
+        $ll1->addFirst(5);
+        $ll1->addFirst(6);
+        $ll1->addFirst(7);
+        $ll1->addFirst(723);
+        $ll1->output();
+        $this->assertEquals(5,$ll1->length());
+    }
+
+    public function testCanRemoveFromLinkedListDoubly(): void
+    {
+        $ll1 = new LinkedListDoubly();
+        $ll1->addFirst(4);
+        $ll1->addFirst(5);
+        $ll1->addFirst(6);
+        $ll1->removeFirst();
+        $ll1->removeFirst();
+        $ll1->output();
+        $this->assertEquals(1,$ll1->length());
+    }
+
+    public function testCanEmptyLinkedListDoubly(): void
+    {
+        $ll1 = new LinkedListDoubly();
+        $ll1->addFirst(4);
+        $ll1->addFirst(5);
+        $ll1->addFirst(6);
+        $ll1->empty();
+        $this->assertEquals(0,$ll1->length());
+    }
+
+    public function testCannotRemoveFromEmptyLinkedListDoubly(): void
+    {
+        $this->expectException(Exception::class);
+        $ll1 = new LinkedListDoubly();
+        $ll1->addFirst(4);
+        $ll1->output();
+        // print_r($ll1->first);
+        // exit;
+        $ll1->removeFirst();
+        
+        $ll1->removeFirst();
+    }
+}
+
